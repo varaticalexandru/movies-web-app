7ee1bb42a784aa17a3e1da67277ab9aaa33ad3a0
alex <varaticalexandru1@gmail.com>
2023-04-13 15:04:53 +0300

    add Home & Hero components
    + implement routing functionality

--- movieclient/movie-hub-v1/src/App.js
+++ movieclient/movie-hub-v1/src/App.js
@@ -1,6 +1,9 @@
 import './App.css';
 import api from './api/axiosConfig';
 import { useState, useEffect } from 'react';
+import Layout from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/home/Home';
 
 function App() {
 
@@ -22,14 +25,23 @@ function App() {
 
   }
 
-  useEffect( () => {
+  useEffect(() => {
     getMovies();
   }, [])
 
 
   return (
     <div className="App">
-    
+      
+      <Routes>
+
+        <Route path="/" element={<Layout/>}>
+          <Route path="/" element={<Home movies={movies} />} ></Route>
+
+        </Route>
+
+      </Routes>
+
     </div>
   );
 }
